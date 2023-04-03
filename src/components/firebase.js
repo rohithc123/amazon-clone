396c557789eed40cfaa35e381dbadead2d3b8f85
Rohith C <rohithc.eec.cseb@gmail.com>
2023-04-03 19:52:20 +0530

    Added firebase snippet

---
+++ src/components/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB9rOTxJ8TpnAeiR2OEhitIrlVDdbpDa0M",
+    authDomain: "clone-20f5b.firebaseapp.com",
+    projectId: "clone-20f5b",
+    storageBucket: "clone-20f5b.appspot.com",
+    messagingSenderId: "977259619750",
+    appId: "1:977259619750:web:b9d872f7ca271e68575b67",
+    measurementId: "G-FB5M0NXS0T"
+  };
