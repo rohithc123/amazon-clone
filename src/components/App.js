94e74dc7a32e441d934367fea26c19926853ba72
Rohith C <rohithc.eec.cseb@gmail.com>
2023-04-06 23:28:58 +0530

    Completed amazon navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 // import logo from './logo.svg';
+import React from 'react';
 import "../styles/App.css";
 import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>Amazon store</h1>
       {/* header */}
       <Header />
       {/* home */}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {default as SearchIcon} from "@mui/icons-material/Search";
 import "../styles/header.css";
 
 function Header() {
@@ -11,20 +12,22 @@ function Header() {
       />
       <div className="header-search">
         <input type="text" className="header-search-input" />
+        {/* <img src="{searchIcon}" alt="" className="header-search-icon" /> */}
+        <SearchIcon className="header-search-icon" />
       </div>
 
       <div className="header-nav">
         <div className="header-option">
-          <span className="header-option-section1">Hello</span>
-          <span className="header-option-secction2">Sign In</span>
+          <span className="header-option-section1">Hello Guest</span>
+          <span className="header-option-section2">Sign In</span>
         </div>
         <div className="header-option">
           <span className="header-option-section1">Returns</span>
-          <span className="header-option-secction2">& Orders</span>
+          <span className="header-option-section2">& Orders</span>
         </div>
         <div className="header-option">
           <span className="header-option-section1">Your</span>
-          <span className="header-option-secction2">Prime</span>
+          <span className="header-option-section2">Prime</span>
         </div>
       </div>
     </div>
