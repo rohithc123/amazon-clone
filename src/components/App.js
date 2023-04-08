297c234460a34e53f4d943e4106c89a360670a73
Rohith C <rohithc.eec.cseb@gmail.com>
2023-04-09 00:18:09 +0530

    Adding Routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
 // import logo from './logo.svg';
-import React from 'react';
+import React from "react";
 import "../styles/App.css";
 import Header from "./Header";
 import Home from "./Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home/>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Header />
+                <Home />
+              </>
+            }
+          />
+          <Route path="/checkout" element={<Header />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
