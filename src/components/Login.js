d25b0dc24626ddf9740c8d455d583dcad3abcead
Rohith C <rohithc.eec.cseb@gmail.com>
2023-04-14 22:02:17 +0530

    Completion for UI of login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React,{useState} from 'react';
 import { NavLink } from "react-router-dom";
 import "../styles/login.css";
 
 function Login() {
+  const [email,setEmail] = useState(' ');
+  const [password,setPassword] = useState(' ');
+
   return (
     <div className="login">
       <NavLink to="/">
@@ -14,6 +17,22 @@ function Login() {
       </NavLink>
       <div className="login-container">
         <h1>Sign In</h1>
+
+        <form>
+          <h5>E-mail</h5>
+          <input type="text" name="" id="" />
+
+          <h5>Password</h5>
+          <input type="password" name="" id="" />
+
+          <button className='login-signin-button'>Sign In</button>
+        </form>
+        <p>
+          By signing-in you agree to Amazon's Clone Conditions of Use & Sale.
+          Please see our Privact Notice,our Cookies Notice and our
+          Interest-Based Ads Notice
+        </p>
+        <button className='login-register-button'>Create your amazon Account</button>
       </div>
     </div>
   );
