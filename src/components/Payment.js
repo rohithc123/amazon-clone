fb7f922ce1acb833ff471825828c80e14a3d779e
Rohith C <rohithc.eec.cseb@gmail.com>
2023-05-21 01:54:51 +0530

    Added Card payment functionality

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,20 +1,44 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Checkoutproduct from "./Checkoutproduct"
+import CurrencyFormat from "react-currency-format";
 import "../styles/Payment.css"
 
+import {NavLink} from "react-router-dom"
 import {useStateValue} from "./Stateprovider";
+import { CardElement,useStripe,useElements} from "@stripe/react-stripe-js";
+import { GetBasketTotal } from "./Reducer";
+
 
 function Payment() {
    const [{ basket, user },dispatch] = useStateValue();
 
+   const stripe = useStripe();
+   const elements = useElements();
+
+   const [error,setError] = useState(null);
+   const [disabled, setDisabled] = useState(true);
+
+   const handleSubmit = e =>{
+    //will add
+   }
+
+   const handleChange = event =>{
+    //listen to cardelement and disply error as customer types
+
+    setDisabled(event.empty);
+    setError(event.error ? event.erroe.message : "");
+   }
+
   return (
     <div className="payment">
         <div className="payment-container">
-            
+            <h1>
+                Checkout (<NavLink to="/checkout">{basket.length} items</NavLink>)
+            </h1>
             {/* payment section - delivery address */}
              <div className="payment-section">
                 <div className="payment-title">
-                    <h3>Delivery Address</h3>
+                    <h3>Delivery Address   </h3>
                 </div>
                 <div className="payment-address">
                     {/* using optional chaining if at all user is undefined */}
@@ -44,10 +68,31 @@ function Payment() {
             
 
             {/* payment section - payment method*/}
-            <div className="payment-section"></div>
-            
-            
+            <div className="payment-section">
+                <div className="payment-title">
+                    <h3>Payment Method</h3>
+                </div>
+                <div className="payment-details">
+                    {/* stripe magic will go here */}
+                    <form onSubmit={handleSubmit}>
+                        <CardElement onChange={handleChange}/>
 
+                        <div className="payment-priceContainer">
+                            <CurrencyFormat
+                            renderText={(value) => (
+                                <h3>Order Total: {value}</h3>
+                            )}
+                            decimalScale={2}
+                            value={GetBasketTotal(basket)}
+                            displayType={"text"}
+                            thousandSeparator={true}
+                            prefix={"₹"}
+                            />
+                        </div>
+                    </form>
+                </div>
+                  
+            </div>
         </div>
     </div>
   )
